70ca50db4253067753fe335298994266a56278fa
scarlett358 <scarlett.j.a.chy@gmail.com>
2026-08-23 16:17:58 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main() 
-{
+int main() {
     string binary;
+    int decimal = 0;
+
     cout << "Enter a binary number: ";
     cin >> binary;
 
-    int decimal = 0;
+    // Check whether every character is either 0 or 1
+    for (char digit : binary) {
+        if (digit != '0' && digit != '1') {
+            cout << "Invalid binary number!" << endl;
+            return 1;
+        }
+    }
 
-    for (char digit : binary) 
-    {
+    // Converts it
+    for (char digit : binary) {
         decimal = decimal * 2 + (digit - '0');
     }
 
